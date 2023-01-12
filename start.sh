83d90135fc3df8313906938f01ad52705742d534
Saholmahla1 <122511578+Saholmahla1@users.noreply.github.com>
2023-01-12 11:28:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
+git clone https://github.com/sahilmahla/MdiskSerchBotV2.git /MdiskSerchBotV2
 cd /MdiskSearchBotV2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
